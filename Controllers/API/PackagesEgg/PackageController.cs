2102b4043e45e56adffb5c5773f7b36f4bf6c48c
Agnes Juliana <agnesjuliana26@gmail.com>
2022-06-02 10:35:41 +0700

    Add PackagesEggController

---
+++ Controllers/API/PackagesEgg/PackageController.cs
@@ -0,0 +1,58 @@
+﻿using EggStore.Domains.Packages.Dto;
+using EggStore.Domains.Packages.Interface;
+using EggStore.Infrastucture.Helpers.ResponseBuilders;
+using EggStore.Infrastucture.Shareds.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EggStore.Controllers.API.Packages
+{
+    [Authorize]
+    [Route("api/packages")]
+    [ApiController]
+    public class PackageController : ControllerBase
+    {
+        private readonly IPackages _IPackages;
+
+        public PackageController(IPackages iPackages)
+        {
+            _IPackages = iPackages;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetPackageList()
+        {
+            var data = await Task.FromResult(_IPackages.FindAll());
+            return Ok(ResponseBuilder.SuccessResponse(ResponseConstant.Detail, data));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetPackageById(Guid id)
+        {
+            var data = await Task.FromResult(_IPackages.FindById(id));
+            return Ok(ResponseBuilder.SuccessResponse(ResponseConstant.Detail, data));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreatePackage(PackagesDto param)
+        {
+            var data = await Task.FromResult(_IPackages.Create(param));
+            return Ok(ResponseBuilder.SuccessResponse(ResponseConstant.Store, data));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdatePackage(PackagesDto param, Guid id)
+        {
+            var data = await Task.FromResult(_IPackages.Update(param, id));
+            return Ok(ResponseBuilder.SuccessResponse(ResponseConstant.Update, data));
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeletePackage(Guid id)
+        {
+            var data = await Task.FromResult(_IPackages.Delete(id));
+            return Ok(ResponseBuilder.SuccessResponse(ResponseConstant.Delete, data));
+        }
+    }
+}
